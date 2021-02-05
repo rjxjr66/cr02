24f255e331fedb36babdf06d10319eb27d3dcb60
hansol <rjxjr55@gmail.com>
2021-02-05 14:03:25 +0900

    가장 가벼운 추도 표시

--- components/Weight.tsx
+++ components/Weight.tsx
@@ -2,7 +2,7 @@ import styles from './Weight.module.css';
 
 export function Weight({ weight, onClick, showAnswer }) {
     return <div className={styles.weight}>
-        {showAnswer && weight.weight === 10?<div className={styles.answer}>내가 제일 무거워!</div>:null}
+        {showAnswer && (weight.weight === 10 || weight.weight === 1)?<div className={styles.answer}>내가 제일 {weight.weight === 10?'무거':'가벼'}워!</div>:null}
         <img onClick={_=>onClick(weight)} key={weight.color} src={`/images/05/weight${weight.color}.png`}></img>
     </div>
 }
